cd819ec05045484d8c2685e2a19dbb48d575ee28
John Gietzen <otac0n@gietzen.us>
2014-04-05 21:05:02 -0700

    Added transposition tables.

--- js/ai.js
+++ js/ai.js
@@ -21,6 +21,7 @@ var compareScores = function(a, b) {
 
 var combineScores = function(scores, weights) {
   var loss = 0;
+  var depth = 0;
   var counts = [];
   var totalWeight = 0;
   for (var i = 0; i < scores.length; i++) {
@@ -30,6 +31,7 @@ var combineScores = function(scores, weights) {
     totalWeight += weight;
 
     loss += score.loss * weight;
+    depth += score.depth;
 
     var c = score.counts;
     var l = c.length;
@@ -43,7 +45,34 @@ var combineScores = function(scores, weights) {
     counts[i] /= totalWeight;
   }
 
-  return { loss: loss / totalWeight, counts: counts };
+  return { loss: loss / totalWeight, counts: counts, depth: (depth / scores.length) + 1 };
+}
+
+var hashes = [];
+for (var x = 0; x < 4; x++) {
+  var a = [];
+  hashes.push(a);
+  for (var y = 0; y < 4; y++) {
+    var b = [];
+    a.push(b);
+    for (var i = 0; i < 17; i++) {
+      b.push(Math.random() * (-1 >>> 0) >>> 0);
+    }
+  }
+}
+hashes[true] = Math.random() * (-1 >>> 0) >>> 0;
+hashes[false] = Math.random() * (-1 >>> 0) >>> 0;
+
+var hashGrid = function(grid) {
+  var hash = hashes[grid.playerTurn];
+  for (var x = 0; x < 4; x++) {
+    for (var y = 0; y < 4; y++) {
+      var cell = grid.cells[x][y];
+      var exp = cell ? Math.log(cell.value) / Math.LN2 : 0;
+      hash = hash ^ hashes[x][y][exp];
+    }
+  }
+  return hash;
 }
 
 var scoreGrid = function(grid) {
@@ -61,7 +90,7 @@ var scoreGrid = function(grid) {
     }
   }
 
-  return { loss: 0, counts: counts };
+  return { loss: 0, counts: counts, depth: 0 };
 }
 
 function AI(grid) {
@@ -70,64 +99,61 @@ function AI(grid) {
 
 AI.prototype.getBest = function () {
   var startTime = +new Date();
-  move = getMove(this.grid, 3);
+  this.cache = {};
+  move = this.getMove(this.grid, 3);
   var endTime = +new Date();
-  console.log([move.score.counts.map(function (x) { var y = x ? x.toFixed(2) : '    '; return y.length < 5 ? ' ' + y : y; }).join(), move.score.loss, endTime - startTime]);
+  console.log([move.score.counts.map(function (x) { var y = x ? x.toFixed(2) : '    '; return y.length < 5 ? ' ' + y : y; }).join(), move.score.loss, move.score.depth.toFixed(1), endTime - startTime]);
   return move;
 }
 
-function getMove(grid, depth) {
-  if (grid.playerTurn && depth == 0) {
-    return { score: scoreGrid(grid) };
-  } else if (grid.playerTurn) {
-    var moves = [];
-    for (var dir = 0; dir < 4; dir++) {
-      var newGrid = grid.clone();
-      if (!newGrid.move(dir).moved) continue;
-      var score = scoreGrid(newGrid);
-      if (score.win) {
-        moves.push({ move: dir, score: score });
+AI.prototype.getMove = function(grid, depth) {
+  var hash = hashGrid(grid);
+  var result = this.cache[hash];
+  if (result && result.score.depth >= depth) {
+    return result;
+  }
+
+  if (grid.playerTurn) {
+    if (depth == 0) {
+      result = { score: scoreGrid(grid) };
+    } else {
+      var moves = [];
+      for (var dir = 0; dir < 4; dir++) {
+        var newGrid = grid.clone();
+        if (!newGrid.move(dir).moved) continue;
+        moves.push({ move: dir, score: this.getMove(newGrid, depth).score });
+      }
+
+      if (moves.length == 0) {
+        var score = scoreGrid(newGrid);
+        score.loss = 1;
+        result = { score: score };
       } else {
-        moves.push({ move: dir, score: getMove(newGrid, depth).score });
+        moves.sort(function (a, b) { return compareScores(a.score, b.score); });
+        result = moves[0];
       }
     }
-
-    if (moves.length == 0) {
-      var score = scoreGrid(newGrid);
-      score.loss = 1;
-      return { score: score };
-    }
-
-    moves.sort(function (a, b) { return compareScores(a.score, b.score); });
-    return moves[0];
   } else {
     var emptyCells = grid.availableCells();
-
-    var pruneFours = false; /*(depth > 2 && emptyCells.length > 2);
-    while (emptyCells.length > 4) {
-      emptyCells.splice(Math.floor(Math.random() * emptyCells.length), 1);
-    }*/
 
     var scores = [];
     var weights = [];
     for (var c = 0; c < emptyCells.length; c++) {
-      var twoGrid = grid.clone();
-      twoGrid.insertTile(new Tile(emptyCells[c], 2));
-      twoGrid.playerTurn = true;
-      scores.push(getMove(twoGrid, depth - 1).score);
-      weights.push(9);
-
-      if (pruneFours) {
-        continue;
-      }
-
       var fourGrid = grid.clone();
       fourGrid.insertTile(new Tile(emptyCells[c], 4));
       fourGrid.playerTurn = true;
-      scores.push(getMove(fourGrid, depth - 1).score);
+      scores.push(this.getMove(fourGrid, depth - 1).score);
       weights.push(1);
+
+      var twoGrid = grid.clone();
+      twoGrid.insertTile(new Tile(emptyCells[c], 2));
+      twoGrid.playerTurn = true;
+      scores.push(this.getMove(twoGrid, depth - 1).score);
+      weights.push(9);
     }
 
-    return { score: combineScores(scores, weights) };
+    result = { score: combineScores(scores, weights) };
   }
+
+  return this.cache[hash] = result;
 }
